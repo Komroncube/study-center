a2547878e4c6df6e652a9828d325e0c84a137ddf
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-30 20:33:59 +0500

    component for teache view

---
+++ src/app/components/admin/teacher/teacher-list/teacher-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './teacher-list.component.html',
+  styleUrl: './teacher-list.component.css'
+})
+export class TeacherListComponent {
+
+}
