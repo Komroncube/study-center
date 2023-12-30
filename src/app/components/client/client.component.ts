dd155d68340dd90c4f98382920f42a40908555e8
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-30 15:55:53 +0500

    clint side component

---
+++ src/app/components/client/client.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-client',
+    templateUrl: './client.component.html'
+})
+
+export class ClientComponent{
+    constructor() { }
+
+}
