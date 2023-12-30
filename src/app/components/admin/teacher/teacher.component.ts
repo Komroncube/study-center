2f6f0f2667e85fe81bca6116b033f148e02801c5
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-30 15:55:11 +0500

    teacher component in admin admin page

---
+++ src/app/components/admin/teacher/teacher.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher',
+  standalone: true,
+  imports: [],
+  templateUrl: `./teacher.component.html`,
+  styleUrl: './teacher.component.css',
+})
+export class TeacherComponent { }
