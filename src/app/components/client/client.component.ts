01df0e5c8c5429d6f32a2c882a3663f7f7cd3661
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-30 20:33:46 +0500

    routing for client component

--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 @Component({
     selector: 'app-client',
+    standalone: true,
+    imports: [RouterLink],
     templateUrl: './client.component.html'
 })
 
---
+++ src/app/components/client/client.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from "@angular/router";
+import { ClientComponent } from "./client.component";
+
+export const CLIENT_ROUTES: Routes = [ 
+    {
+        path:'',
+        component: ClientComponent,
+        title: "Client page"
+
+    },
+    
+]
