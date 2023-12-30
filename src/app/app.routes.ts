fe0e3b8ab457a0846524bde01f10c40e88234e24
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-30 15:54:56 +0500

    routing for basic files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { ClientComponent } from './components/client/client.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'client',
+        pathMatch: 'full'
+    },
+    { 
+        path: 'admin', 
+        component: AdminComponent,
+        title:"Admin sahifasi"
+    },
+    { 
+        path: 'client', 
+        component: ClientComponent,
+        title: "Client sahifasi"
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
+];
