8ccf4b2368397f3586e22f7b06b1a262c7c411e0
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-31 20:05:59 +0500

    teacher interface

---
+++ src/app/components/admin/teacher/models/teachers.model.ts
@@ -0,0 +1,15 @@
+export interface ITeacherResponse {
+    id:number, 
+    name:string,
+    description:string,
+    address:string,
+    dateOfBirth:string, //date
+    dateOfRegister:string, //date
+    phone:string,
+    email:string,
+    telegramUserName:string,
+    specialization:string
+}
+export interface ITeacherRequest {
+
+}
