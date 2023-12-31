e5b20443f2c117e717193f79f387ac44be3057e1
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-31 15:59:56 +0500

    teacher list table added

--- src/app/components/admin/teacher/teacher-list/teacher-list.component.ts
+++ src/app/components/admin/teacher/teacher-list/teacher-list.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule} from 'ng-zorro-antd/divider'
 
+interface Person {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+}
 @Component({
   selector: 'app-teacher-list',
   standalone: true,
-  imports: [],
+  imports: [NzDividerModule, NzTableModule],
   templateUrl: './teacher-list.component.html',
   styleUrl: './teacher-list.component.css'
 })
 export class TeacherListComponent {
-
+  listOfData: Person[] = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park'
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park'
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park'
+    }
+  ];
 }
