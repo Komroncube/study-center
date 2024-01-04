d5dc76edcf87b093abec93690db7812d45bb6e9c
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-04 16:01:33 +0500

    get by id method added

--- src/app/components/admin/teacher/services/teachers.services.ts
+++ src/app/components/admin/teacher/services/teachers.services.ts
@@ -7,21 +7,24 @@ import { ITeacherRequest, ITeacherResponse } from "../models/teachers.model";
     providedIn: 'root'
 })
 export class TeacherService {
-    url='api/Teachers'
+    
     /**
      * 
      * @param http 
      */
     constructor(private http: HttpClient) { }
-    fullurl = 'http://127.0.0.1:5018/api/Teachers'
+    url = 'http://127.0.0.1:5018/api/Teachers'
     getAll() {
-        return this.http.get<ITeacherResponse[]>(this.fullurl, {headers: new HttpHeaders({'Access-Control-Allow-Origin':'*'})})
+        return this.http.get<ITeacherResponse[]>(this.url, {headers: new HttpHeaders({'Access-Control-Allow-Origin':'*'})})
     }
     add(model:ITeacherRequest) {
         const headers = new HttpHeaders()
         .set('Content-Type', 'application/json')
         .set('Access-Control-Allow-Origin', '*');
         console.log(model);
-        return this.http.post<ITeacherResponse>(this.fullurl, JSON.stringify(model), {headers })
+        return this.http.post<ITeacherResponse>(this.url, JSON.stringify(model), {headers })
+    }
+    getById(id:number){
+        return this.http.get<ITeacherResponse>(`${this.url}/${id}`)
     }
 }
