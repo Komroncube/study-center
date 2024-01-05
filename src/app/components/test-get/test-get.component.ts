51ee89edf8fc8f901cb100436a2b742defe885af
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-05 15:50:41 +0500

    testing primeng

--- src/app/components/test-get/test-get.component.ts
+++ src/app/components/test-get/test-get.component.ts
@@ -1,24 +1,148 @@
-import { Component } from '@angular/core';
-import { TeacherService } from '../admin/teacher/services/teachers.services';
+import { Component, OnInit } from '@angular/core';
 import { ITeacherResponse } from '../admin/teacher/models/teachers.model';
-
+import { FooterMacComponent } from "../footer-mac/footer-mac.component";
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
+import { AccordionModule } from 'primeng/accordion';
 @Component({
-  selector: 'app-test-get',
-  standalone: true,
-  imports: [],
-  templateUrl: './test-get.component.html',
-  styleUrl: './test-get.component.css'
+    selector: 'app-test-get',
+    standalone: true,
+    templateUrl: './test-get.component.html',
+    styleUrl: './test-get.component.css',
+    imports: [FooterMacComponent, MenubarModule, InputTextModule, AccordionModule]
 })
-export class TestGetComponent {
+export class TestGetComponent implements OnInit {
   teachers! : ITeacherResponse[]
   json!:string
   /**
    *
    */
-  constructor(private teacherService: TeacherService) {
-    this.teacherService.getAll().subscribe(t => {
-      this.teachers = t
-      this.json = JSON.stringify(this.teachers)
-    })
-  }
+  items: MenuItem[] | undefined;
+  value:string|undefined;
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'File',
+                icon: 'pi pi-fw pi-file',
+                items: [
+                    {
+                        label: 'New',
+                        icon: 'pi pi-fw pi-plus',
+                        items: [
+                            {
+                                label: 'Bookmark',
+                                icon: 'pi pi-fw pi-bookmark'
+                            },
+                            {
+                                label: 'Video',
+                                icon: 'pi pi-fw pi-video'
+                            }
+                        ]
+                    },
+                    {
+                        label: 'Delete',
+                        icon: 'pi pi-fw pi-trash'
+                    },
+                    {
+                        separator: true
+                    },
+                    {
+                        label: 'Export',
+                        icon: 'pi pi-fw pi-external-link'
+                    }
+                ]
+            },
+            {
+                label: 'Edit',
+                icon: 'pi pi-fw pi-pencil',
+                items: [
+                    {
+                        label: 'Left',
+                        icon: 'pi pi-fw pi-align-left'
+                    },
+                    {
+                        label: 'Right',
+                        icon: 'pi pi-fw pi-align-right'
+                    },
+                    {
+                        label: 'Center',
+                        icon: 'pi pi-fw pi-align-center'
+                    },
+                    {
+                        label: 'Justify',
+                        icon: 'pi pi-fw pi-align-justify'
+                    }
+                ]
+            },
+            {
+                label: 'Users',
+                icon: 'pi pi-fw pi-user',
+                items: [
+                    {
+                        label: 'New',
+                        icon: 'pi pi-fw pi-user-plus'
+                    },
+                    {
+                        label: 'Delete',
+                        icon: 'pi pi-fw pi-user-minus'
+                    },
+                    {
+                        label: 'Search',
+                        icon: 'pi pi-fw pi-users',
+                        items: [
+                            {
+                                label: 'Filter',
+                                icon: 'pi pi-fw pi-filter',
+                                items: [
+                                    {
+                                        label: 'Print',
+                                        icon: 'pi pi-fw pi-print'
+                                    }
+                                ]
+                            },
+                            {
+                                icon: 'pi pi-fw pi-bars',
+                                label: 'List'
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                label: 'Events',
+                icon: 'pi pi-fw pi-calendar',
+                items: [
+                    {
+                        label: 'Edit',
+                        icon: 'pi pi-fw pi-pencil',
+                        items: [
+                            {
+                                label: 'Save',
+                                icon: 'pi pi-fw pi-calendar-plus'
+                            },
+                            {
+                                label: 'Delete',
+                                icon: 'pi pi-fw pi-calendar-minus'
+                            }
+                        ]
+                    },
+                    {
+                        label: 'Archieve',
+                        icon: 'pi pi-fw pi-calendar-times',
+                        items: [
+                            {
+                                label: 'Remove',
+                                icon: 'pi pi-fw pi-calendar-minus'
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                label: 'Quit',
+                icon: 'pi pi-fw pi-power-off'
+            }
+        ];
+    }
 }
