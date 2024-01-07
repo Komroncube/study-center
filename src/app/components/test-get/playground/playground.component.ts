33598a2c2f40b646a4a569f6fac92de22a13cc83
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2024-01-07 20:31:08 +0500

    Update playground component with input and button styling

--- src/app/components/test-get/playground/playground.component.ts
+++ src/app/components/test-get/playground/playground.component.ts
@@ -1,19 +1,42 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { KeyFilterModule } from 'primeng/keyfilter';
 import { InputTextModule } from 'primeng/inputtext';
+import { ChildDemoComponent } from "../child-demo/child-demo.component";
 @Component({
-  selector: 'app-playground',
-  standalone: true,
-  imports: [InputTextModule, ReactiveFormsModule],
-  templateUrl: './playground.component.html',
-  styleUrl: './playground.component.css'
+    selector: 'app-playground',
+    standalone: true,
+    templateUrl: './playground.component.html',
+    styleUrl: './playground.component.css',
+    imports: [InputTextModule, ReactiveFormsModule, KeyFilterModule, FormsModule, ChildDemoComponent]
 })
 export class PlaygroundComponent {
-  value: string|undefined;
+  /**
+   * 
+   */
+  increase() {
+    this.childCounter += 1
+  }
+    /**
+   * 
+   */
+    text :string = "Hello World"
+  
   form: FormGroup = new FormGroup({
-    word: new FormControl('')
+    // word: new FormControl('')
   })
   submit() {
-    window.alert(this.value)
+    window.alert(this.text)
+  }
+  
+  counter :number = 3;
+  childCounter: number = 0;
+
+  /**
+   * 
+   * @param $event 
+   */
+  handleChange($event: number) {
+    this.childCounter = $event
   }
 }
